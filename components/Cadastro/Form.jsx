ba8e613ffdbf0721d9e55dc38d5d150a4b179a0e
Silva Joao (CaP/ETS) <slj4ca@bosch.com>
2022-11-21 14:17:26 -0300

    refaturacao da pagina de cadastro

--- components/Cadastro/Form.jsx
+++ components/Cadastro/Form.jsx
@@ -6,10 +6,12 @@ export default function Form(){
     return(
         <form className={Styles.container}>
             <h1>Cadastro</h1>
-            <input type="text" placeholder='Nome completo'/>
-            <input type="number" placeholder='CPF'/>
-            <input type="email" placeholder='E-mail'/>
-            <input type="email" placeholder='Repetir E-mail'/>
+            <div className={Styles.content}>
+                <input className={Styles.inputUpper} type="text" placeholder='Nome completo'/>
+                <input type="number" placeholder='CPF'/>
+                <input type="email" placeholder='E-mail'/>
+                <input className={Styles.inputDown} type="email" placeholder='Repetir E-mail'/>
+            </div>
             <button type='submit'>CONTINUAR</button>
             <a>JÁ SOU CLIENTE</a>
         </form>
