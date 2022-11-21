9849e5c4ab968422489e523b8d34cbcbff0287f4
Silva Joao (CaP/ETS) <slj4ca@bosch.com>
2022-11-21 15:07:04 -0300

    Adicionando card no componente wallpaper da pagina home

--- components/home/Wallpaper.jsx
+++ components/home/Wallpaper.jsx
@@ -4,7 +4,9 @@ export default function Wallpaper(){
     return(
         <>
             <div className={Styles.container}>
-                <h1>Ola Mundo</h1>
+                <div className={Styles.card}>
+                    <h1>Conte a sua história e impulsione seus sonhos com a conta BooBanck.</h1>
+                </div>
             </div>
         </>
     )
